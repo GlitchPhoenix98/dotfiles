c509bf06159f2ed7d16623649b876d753a319fe9
Glitch <78001268+GlitchPhoenix98@users.noreply.github.com>
2023-04-25 00:04:21 +0000

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-mv dots/bash_profile ~/.bash_profile &
-mv dots/bashrc ~/.bashrc &
+mv dots/bash_profile ~/.bash_profile &&
+mv dots/bashrc ~/.bashrc &&
 mkdir ~/.config &
-mv dots/gtk-3.0 ~/.config/gtk-3.0 &
-mv dots/xinitrc ~/.xinitrc &
-mv dots/wallpapers ~/.config/wallpapers &
-echo "Done! Now all you need is to manually move the user.js for firefox and compile everything in the suckless folder!"
+mv dots/gtk-3.0 ~/.config/gtk-3.0 &&
+mv dots/xinitrc ~/.xinitrc &&
+mv dots/wallpapers ~/.config/wallpapers &&
+cd suckless/dwm && sudo make clean install &&
+cd .. && cd slstatus && sudo make clean install &&
+cd .. && cd st && sudo make clean install &&
+printf  "\n\nInstall completed, all that is required is movement of the firefox user.js\n"
