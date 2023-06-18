3ebf25ca9ffd1609fca4f612b07f3b342482effc
Glitch <78001268+GlitchPhoenix98@users.noreply.github.com>
2023-06-18 02:11:36 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 mv dots/bash_profile ~/.bash_profile &&
 mv dots/bashrc ~/.bashrc &&
 mkdir ~/.config &
-#This line below doesn't work for some reason and I have no idea why, if someone could help me out that would be great
-#mv 'dots/gtk-3.0' ~/.config/gtk-3.0 &&
+mv 'dots/gtk-3.0' ~/.config/gtk-3.0 &&
 mv dots/xinitrc ~/.xinitrc &&
 mv dots/wallpapers ~/.config/wallpapers &&
 cd suckless/dwm && sudo make clean install &&
 cd .. && cd slstatus && sudo make clean install &&
 cd .. && cd st && sudo make clean install &&
-mv dots/mutt ~/.config/mutt
+mv dots/mutt ~/.config/mutt &&
 printf  "\n\nInstall completed, all that is required is movement of the firefox user.js and gtk settings.\n"
